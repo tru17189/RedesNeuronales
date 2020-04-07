dffb2841bf428c6c248a357b93af3f498d644136
tru17189 <32879914+tru17189@users.noreply.github.com>
2020-04-07 00:35:41 -0600

    tarea

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\axelt\documents\uvg4\inteligenciaartificial\redesneuronales\redesneuronales-env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
